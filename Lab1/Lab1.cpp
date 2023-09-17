757f0afbdbd3fd4a722dd91da04e54b89ef5607f
arlenochka <m.nupreichik@yandex.ru>
2023-09-17 22:48:10 +0300

    1-4 items written (not tested)

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Elem 
 {
 public:
-	int number; // число, хранящееся в элементе
+	int* number; // число, хранящееся в элементе
 	Elem* next; // ссылка на следующий элемент
 	Elem(int number);
 };
@@ -45,13 +45,14 @@ public:
 class OneLinkList 
 {
 public:
-	Elem* head;
+	Elem* head; Elem* tail;
+	
 	OneLinkList();
 
-	void insLast(int toInsert);						// 1.	добавление в конец списка
-	void insFirst(int toInsert);					// 2.	добавление в начало списка
-	void delLast();									// 3.	удаление последнего элемента
-	void delFirst();								// 4.	удаление первого элемента
+	void insLast(int toInsert);						// 1.	добавление в конец списка +
+	void insFirst(int toInsert);					// 2.	добавление в начало списка +
+	void delLast();									// 3.	удаление последнего элемента +
+	void delFirst();								// 4.	удаление первого элемента +
 	void insertBefore(int index);					// 5.	добавление элемента по индексу
 	void getElem(int index);						// 6.	получение элемента по индексу
 	void deleteElem(int index);						// 7.	удаление элемента по индексу
@@ -67,6 +68,7 @@ public:
 	void findFirstEntry(OneLinkList toFind);		// 17.	поиск первого вхождения другого списка в список
 	void findLastEntry(OneLinkList toFind);			// 18.	поиск последнего вхождения другого списка в список
 	void changeTwoByIndex(int index1, int index2);  // 19.	обмен двух элементов списка по индексам
+	
 	void printData();
 
 	~OneLinkList();
@@ -78,35 +80,93 @@ int main()
     cout << "Hello World!\n";
 }
 
-Elem::Elem(int number) 
+Elem::Elem(int numb) 
 {
-	this->number = number;
+	this->number = new int; 
+	*(this->number) = numb;
 	this->next = NULL;
 };
 
 OneLinkList::OneLinkList()
 {
 	this->head = NULL;
+	this->tail = NULL;
 }
 
 void OneLinkList::insLast(int toInsert)
 {
+	Elem* elem = new Elem(toInsert);
+	if (head == NULL) head = elem;
+	if (tail != NULL) tail->next = elem;
+	tail = elem;
 }
 
 void OneLinkList::insFirst(int toInsert)
 {
+	Elem* elem = new Elem(toInsert);
+	if (head == NULL) head = elem;
+	else {
+		elem->next = head;
+		head = elem;
+	}
 }
 
 void OneLinkList::delLast()
 {
+	Elem* preLast = this->head;
+
+	if (preLast == NULL) cout << "List is empty.\n";
+	else {
+		// переходим к предпоследнему элементу
+		while (preLast->next != this->tail && preLast->next != NULL) {
+			preLast = preLast->next;
+		}
+
+		// проверка на единственность элемента в списке
+		if (preLast == head) {
+			cout << "You're deleting the only element.\n";
+			delete preLast;
+		}
+		else {
+			delete preLast->next;
+		}
+	}
 }
 
 void OneLinkList::delFirst()
 {
+	if (head == NULL) return;
+	if (head == tail) { // один элемент в односвязном списке
+		delete tail;
+		head = NULL; tail = NULL;
+		return;
+	}
+	// в списке 2 и более элементов
+	Elem* elem = head;
+	head = elem->next;
+	delete elem;
+
 }
 
 void OneLinkList::insertBefore(int index)
 {
+	if (index < 0) {
+		cout << "Index must be positive.\n";
+		return;
+	}
+	
+	int indexCounter = 0;
+	Elem* currentELem = head;
+
+	// идем к указателю на 
+	while (currentELem != NULL && index > indexCounter) {
+		currentELem = currentELem->next;
+		indexCounter++;
+	}
+
+	// 2 случая: индекс больше чем длина списка - 1 ИЛИ все нормально
+
+	// if (currentELem == NULL && index)
 }
 
 void OneLinkList::getElem(int index)
